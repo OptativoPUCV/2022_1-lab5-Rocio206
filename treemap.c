49e02e63a89d91dd90ee53bb2674b3e9138f064d
Your Name <you@example.com>
2022-04-24 18:21:08 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -86,6 +86,7 @@ TreeNode *minimum(TreeNode *x)
 void removeNode(TreeMap *tree, TreeNode *node)
 {
     if (node->left != NULL && node->right != NULL){
+        printf("----------");
         TreeNode *max = node->left;
         while(max->right != NULL){
             max = max->right;
@@ -101,7 +102,7 @@ void removeNode(TreeMap *tree, TreeNode *node)
         free(max);
     }
     else{
-        printf("----------");
+        
         if (node->left != NULL)
         {
             node->parent->left = node->left;
