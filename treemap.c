2165cb5744ddd66b77501fe1e575838ce3ffeb21
Your Name <you@example.com>
2022-04-24 22:05:47 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -68,7 +68,12 @@ void insertTreeMap(TreeMap *tree, void *key, void *value)
                 tree->current = tree->current->left;
         }
     }
-    tree->current = createTreeNode(key, value);
+    tree->current = tree->current->parent;
+    if (tree->lower_than(tree->current->pair->key, key) == 1)
+        tree->current->right = createTreeNode(key, value);
+
+    else
+        tree->current->left = createTreeNode(key, value);
 }
 
 TreeNode *minimum(TreeNode *x)
